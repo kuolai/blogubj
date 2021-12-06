f9c393f3893d1683e538a3e09d6bc68b77c6702e
kuolai <kuolaisha@gmail.com>
2021-12-06 22:05:41 +0800

    no minima theme

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,3 +1,5 @@
+const plugin = require('tailwindcss/plugin')
+
 module.exports = {
   purge: [],
   darkMode: false, // or 'media' or 'class'
@@ -7,5 +9,16 @@ module.exports = {
   variants: {
     extend: {},
   },
-  plugins: [],
+  plugins: [
+    require('@tailwindcss/typography'),
+    plugin(function({ addBase, theme }) {
+      addBase({
+        'h1': { fontSize: theme('fontSize.2xl') },
+        'h2': { fontSize: theme('fontSize.xl') },
+        'h3': { fontSize: theme('fontSize.lg') },
+        'h4': { fontSize: theme('fontSize.md') },
+      })
+    })
+  ],
+
 }
